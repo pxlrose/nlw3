2618af221fc6e953566ffcbe431b2d5ce1bf3b9f
Pixel Rose <pixelrosecat@gmail.com>
2020-10-24 00:41:50 -0300

    aula 4 pt 1

---
+++ src/server.js
@@ -0,0 +1,18 @@
+//importar dependencia
+const express = require('express');
+const path = require('path');
+
+//iniciando o express
+const server = express();
+
+server
+// utilizando os arquivos estáticos
+.use(express.static('public'))
+
+//criar uma rota
+.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//ligar servidor
+server.listen(5500)
